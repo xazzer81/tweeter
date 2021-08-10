e213c983f501de571aca5043392202411b9bc5c7
Ahmed <achau1019@gmail.com>
2021-08-10 23:02:36 +0000

    Add counter functionality, and it turns red to indicate exceeded character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    const str = $(this).val()
+    const len = str.length; // Get string length inside tweet area.
+    const counter = $(this).next()  //Get the counter node
+      .children()
+      .next();
+    
+    // Make counter red if its less than 0.
+    if (counter.html() < 0) {
+      $(counter).addClass('illegal');
+    } else {
+      $(counter).removeClass('illegal');
+    }
+
+    counter.html(140 - len);
+  });
+});
+
