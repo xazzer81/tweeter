5a06f800f4afe95e9c08832a6c923a7d7715f948
Ahmed <achau1019@gmail.com>
2021-08-11 17:28:25 +0000

    Add render tweet function that renders all tweets in database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,19 +3,43 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
 $( document ).ready(function() {
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for(const tweet of tweets) {
+      const tweetElement = createTweetElement(tweet);
+      $('.all-tweets').append(tweetElement);
+    }
+  }
+
   // Create a new tweet element. ---- Using time ago library to format the time.
   const createTweetElement = function(tweetData) {
     const markup = `
@@ -44,5 +68,6 @@ $( document ).ready(function() {
     `
     return markup;
   }
-  console.log(createTweetElement(tweetData));
+  
+  renderTweets(data);
 });
