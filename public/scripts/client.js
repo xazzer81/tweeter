db94d5ffa62776166acfccfdf087c401c13e1c8d
Ahmed <achau1019@gmail.com>
2021-08-11 17:12:59 +0000

    Add createNewElement method to create a new tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,48 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
 
+$( document ).ready(function() {
+  // Create a new tweet html element
+  const createTweetElement = function(tweetData) {
+    const markup = `
+      <article class='tweet'>
+        <header>
+          <div id='leftHead'>
+            <img src=${tweetData['user'].avatars}>
+            <h3 class='name'>${tweetData['user'].name}</h3> 
+          </div>
+          <div id='righHead'>
+            <h3 class='handle'>${tweetData['user'].handle}</h3>
+          </div>
+        </header>
+        <p>${tweetData['content'].text}</p>
+        <footer>
+          <div class = 'footer-text'>
+            <span class='time'>${tweetData.created_at}</span>
+            <span class='icons'>
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+            </span>
+          </div> 
+        </footer>
+      </article>
+    `
+    return markup;
+  }
+
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+});
